c903e257df97498e71ffe4813f1c83726dfd3a73
arrrriieeelll <arieldwisaputra@gmail.com>
2024-10-31 20:40:09 +0700

    upload to new branch

---
+++ modul3/KalkulatorMakanan.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class KalkulatorMakanan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah kalori nasi (per porsi): ");
+        int kaloriNasi = scanner.nextInt();
+
+        System.out.print("Masukkan jumlah kalori ayam (per porsi): ");
+        int kaloriAyam = scanner.nextInt();
+
+        // Menggunakan custom live template "totalKalori"
+        int totalKalori = kaloriNasi + kaloriAyam; 
+
+        System.out.println("Total kalori: " + totalKalori);
+    }
+}
