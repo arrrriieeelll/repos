1e548dd5f99c62b61a7a6bf31c434176de43981e
arrrriieeelll <arieldwisaputra@gmail.com>
2024-11-01 13:51:50 +0700

    NEW folder

---
+++ modul4/PemesananTiket.java
@@ -0,0 +1,115 @@
+
+/**
+ * Kelas Pesawat merepresentasikan data sebuah pesawat.
+ */
+class Pesawat {
+    /** Kode pesawat (misal: GA123) */
+    String kodePesawat;
+    /** Nama pesawat (misal: Boeing 737) */
+    String namaPesawat;
+    /** Jumlah kursi yang tersedia */
+    int jumlahKursi;
+
+    /**
+     * Konstruktor untuk membuat objek Pesawat.
+     *
+     * @param kode Kode pesawat
+     * @param nama Nama pesawat
+     * @param kursi Jumlah kursi
+     */
+    Pesawat(String kode, String nama, int kursi) {
+        this.kodePesawat = kode;
+        this.namaPesawat = nama;
+        this.jumlahKursi = kursi;
+    }
+}
+
+/**
+ * Kelas Penerbangan merepresentasikan data sebuah penerbangan.
+ */
+class Penerbangan {
+    /** Nomor penerbangan */
+    String nomorPenerbangan;
+    /** Rute penerbangan (misal: Jakarta - Bali) */
+    String rute;
+    /** Tanggal keberangkatan */
+    String tanggalKeberangkatan;
+    /** Waktu keberangkatan */
+    String waktuKeberangkatan;
+    /** Pesawat yang digunakan */
+    Pesawat pesawat;
+
+    // ... (sisanya sama seperti kode sebelumnya)
+}
+
+/**
+ * Kelas utama untuk melakukan pemesanan tiket pesawat.
+ */
+public class PemesananTiket {
+    /**
+     * Metode utama untuk menjalankan program.
+     *
+     * @param args Argumen baris perintah (tidak digunakan)
+     */
+    public static void main(String[] args) {
+        // ... (sisanya sama seperti kode sebelumnya)
+    }
+}
+ 
+/**
+ * Kelas Pesawat merepresentasikan data sebuah pesawat.
+ */
+class Pesawat {
+    /** Kode pesawat (misal: GA123) */
+    String kodePesawat;
+    /** Nama pesawat (misal: Boeing 737) */
+    String namaPesawat;
+    /** Jumlah kursi yang tersedia */
+    int jumlahKursi;
+
+    /**
+     * Konstruktor untuk membuat objek Pesawat.
+     *
+     * @param kode Kode pesawat
+     * @param nama Nama pesawat
+     * @param kursi Jumlah kursi
+     */
+    Pesawat(String kode, String nama, int kursi) {
+        this.kodePesawat = kode;
+        this.namaPesawat = nama;
+        this.jumlahKursi = kursi;
+    }
+}
+
+/**
+ * Kelas Penerbangan merepresentasikan data sebuah penerbangan.
+ */
+class Penerbangan {
+    /** Nomor penerbangan */
+    String nomorPenerbangan;
+    /** Rute penerbangan (misal: Jakarta - Bali) */
+    String rute;
+    /** Tanggal keberangkatan */
+    String tanggalKeberangkatan;
+    /** Waktu keberangkatan */
+    String waktuKeberangkatan;
+    /** Pesawat yang digunakan */
+    Pesawat pesawat;
+
+    // ... (sisanya sama seperti kode sebelumnya)
+}
+
+/**
+ * Kelas utama untuk melakukan pemesanan tiket pesawat.
+ */
+public class PemesananTiket {
+    /**
+     * Metode utama untuk menjalankan program.
+     *
+     * @param args Argumen baris perintah (tidak digunakan)
+     */
+    public static void main(String[] args) {
+        // ... (sisanya sama seperti kode sebelumnya)
+    }
+}
+ 
